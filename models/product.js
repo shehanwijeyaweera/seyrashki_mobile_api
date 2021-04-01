baf42823a60658ad063a29d2708d01df1926e988
Shehan Wijeyaweera <shehan.naveendra@gmail.com>
2021-04-01 17:32:02 +0530

    mongodb database configured. models files created without implementation. routes configured.

---
+++ models/product.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: String,
+    countInStock: {
+        type: Number,
+        required: true
+    }
+})
+
+exports.Product = mongoose.model('Product', productSchema);
