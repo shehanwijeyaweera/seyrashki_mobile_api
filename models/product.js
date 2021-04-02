dd8aa9cba3ccb52cf463607693839a2d481cef7e
Shehan Wijeyaweera <shehan.naveendra@gmail.com>
2021-04-02 13:00:13 +0530

    category and product models updated. all of the category CRUD routes were implemented

--- models/product.js
+++ models/product.js
@@ -8,11 +8,47 @@ const productSchema = mongoose.Schema({
     description: {
         type: String,
         required: true
+    },richDescription: {
+        type: String,
+        default: ''
     },
-    image: String,
+    image: {
+        type: String,
+        default: ''
+    },imgaes: [{
+        type: String
+    }],
+    brand:{
+        type: String,
+        default: ''
+    },price: {
+        type:Number,
+        default: 0
+    },category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    }
+    ,
     countInStock: {
         type: Number,
-        required: true
+        required: true,
+        min:0,
+        max:100
+    }, rating: {
+        type: Number,
+        default: 0
+    },numReviews:{
+        type: Number,
+        default: 0
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
     }
 })
 
