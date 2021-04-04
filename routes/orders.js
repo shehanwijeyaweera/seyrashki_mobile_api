a2a2dcd43662a98ab05cc4ef1a0dea52fbc7192e
Shehan Wijeyaweera <shehan.naveendra@gmail.com>
2021-04-04 20:06:59 +0530

    implemented get total sales, get total order count, user get all orders functions in orders routes

--- routes/orders.js
+++ routes/orders.js
@@ -115,4 +115,42 @@ router.delete('/:id', (req, res) => {
     })
 })
 
+//get total sales of the shop
+router.get('/get/totalsales', async (req, res ) => {
+    const totalSales = await Order.aggregate([
+        { $group: { _id: null , totalsales : { $sum : '$totalPrice'}}}
+    ])
+
+    if(!totalSales){
+        return res.status(400).send('The order sales cannot be generated')
+    }
+
+    res.send({ totalsales: totalSales.pop().totalsales })
+})
+
+//get total order count
+router.get(`/get/count`, async (req, res) => {
+    const orderCount = await Order.countDocuments((count) => count)
+  
+    if (!orderCount) {
+      res.status(500).json({ success: false });
+    }
+  
+    res.send({ orderCount: orderCount });
+  });
+
+  //get all orders of a specific user
+router.get(`/get/userorders/:userid`, async (req, res) => {
+    const userOrderList = await Order.find({ user: req.params.userid })
+      .populate({path: 'orderItems', populate: {
+          path: 'product', populate: 'category'
+      }})
+      .sort({ dateOrdered: -1 }); //sort the results from newst to oldest
+  
+    if (!userOrderList) {
+      res.status(500).json({ success: false });
+    }
+    res.send(userOrderList);
+  });
+
 module.exports = router;
